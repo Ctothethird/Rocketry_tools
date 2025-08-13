f6158f7d0ac09d801d5cba24c919b0f6b1d57747
BAGELBOI <liamdbriley@icloud.com>
2025-08-12 17:29:39 -0700

    Update pressureratio.py

--- pressureratio.py
+++ pressureratio.py
@@ -4,11 +4,15 @@ import json
 airpressure = 0.0
 fueltankarea = 0.0
 nosslearea = 0.0
-fueltanksize = 0.0
+fueltankvolume = 0.0
 
 #functions
-def simulate():
+def simulate(a, b, c, d):
     pass
 
 while True:
-    simulate()
+    airpressureinput = input("What is the air pressure? ")
+    fueltankareainput = input("What is the fuel tank area(in square units)? ")
+    nossleareainput = input("What is the nossle area(in square units)? ")
+    fueltankvolumeinput = input("What is the fuel tank volume(in cubic units)? ")
+    simulate(airpressureinput, fueltankareainput, nossleareainput, fueltankvolumeinput)
