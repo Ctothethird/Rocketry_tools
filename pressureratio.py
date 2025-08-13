84754ef0902004041346f56a9ab5fc7f26e64eae
BAGELBOI <liamdbriley@icloud.com>
2025-08-12 17:51:56 -0700

    Update pressureratio.py

--- pressureratio.py
+++ pressureratio.py
@@ -7,17 +7,21 @@ nosslearea = 0.0
 fueltankvolume = 0.0
 testwatercount = 0.0
 testaircount = 0.0
+
 #functions
-def simulate(a, b, c, d):
-    pass
+def simulate(p, Fa, Na, Fv):
+    p/Fa = f
 
 while True:
-    airpressureinput = input("What is the air pressure? ")
+    airpressureinput = input("What is the air pressure(in pascals)? ")
     fueltankareainput = input("What is the fuel tank area(in square units)? ")
     nossleareainput = input("What is the nossle area(in square units)? ")
     fueltankvolumeinput = input("What is the fuel tank volume(in cubic units)? ")
-    while true: #actuall loop
+
+    while True: #actuall loop
         testaircount = fueltankvolume - testwatercount
         
         #stroke up steps:
         testwatercount = testwatercount + 0.1
+        
+        simulate(airpressureinput, fueltankareainput, nossleareainput, fueltankvolumeinput)
