e8637d534e8f447dc9ec56fcd821235ba656a868
BAGELBOI <liamdbriley@icloud.com>
2026-04-14 22:54:13 -0700

    Pretty much all of water amount data is done
    It takes in your water amount and then it outputs a velocity and dynamic pressure(second one isnt important) I will explain once i return. The math is all correct i just havent been able to test my code because they banned replit. also i realized that it would glitch the fuck out if i didnt do a float so I asked ai to make me a quick check for that didnt check that at all.

--- WaterAmount.py
+++ WaterAmount.py
@@ -1,1 +1,37 @@
+import math
 
+litertometer = 1e-3
+grav = 9.81
+density = 1.225
+atmos = 101325 
+
+PressureInitial = 551580.6
+VolumeFinal = 0.002 #in meter^3
+
+def dobetter(prompt):
+  while True:
+    try:
+      val = float(input(prompt))
+      if val > 0:
+        return val
+      print("Dude it gotta be not negative be smarter yo")
+    except ValueError:
+      print("Dude you put a number in frickin idiot yo")
+
+def main():
+  VolumeInitial = dobetter("Water Amount: ")
+  VolumeInitial* = (2-VolumeInitial) * litertometer
+
+#actual calculations
+Pressure_Final = (PressureInitial * VolumeInitial*) / VolumeFinal
+
+airdens = density * (Pressure_Final / atmos)
+
+DynamicPressure = PressureInitial - Pressure_Final
+
+PressureTotal = PressureInitial + DynamicPressure
+
+v = math.sqrt(2.0 * DynamicPressure / airdens)
+
+print("Velocity(in m/s): " + string(v))
+print("DynamicPressure(in Pa): " + string(DynamicPressure))
