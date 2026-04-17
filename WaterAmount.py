894fd105b0617ce416094f33351192c70a5d2b6f
BAGELBOI <liamdbriley@icloud.com>
2026-04-16 17:31:46 -0700

    Refactor main function for water pressure calculations
    Mass is for 2 bottles, a 65 gramme egg, and 65.75 cm^2 by 5mm of cardboard

--- WaterAmount.py
+++ WaterAmount.py
@@ -1,27 +1,55 @@
 import math
+import string
 
 litertometer = 1e-3
 grav = 9.81
 density = 1.225
 atmos = 101325 
+bottlemass = 0.1779
+Area = 615.75e-6
+Time𐤃= 0.0001
 
 PressureInitial = 551580.6
 VolumeFinal = 0.002 #in meter^3
 
 def main():
-  VolumeInitial = input("Water Amount: ")
-  VolumeInitial* = (2-VolumeInitial) * litertometer
+  VolumeInitial = float(input("Water Amount: "))
+  Volumemeters = VolumeInitial * litertometer
+  watermass = VolumeInitial 
+
+  airvolume = VolumeFinal - Volumemeters
+  P_current = PressureInitial * (airvolume / VolumeFinal)
 
 #actual calculations
-  Pressure_Final = (PressureInitial * VolumeInitial*) / VolumeFinal
+  Pressure_Final = (PressureInitial * airvolume) / VolumeFinal
+  v = 0.0
 
-  airdens = density * (Pressure_Final / atmos)
+  while P_current > atmos and Volumemeters > 0:
 
-  DynamicPressure = PressureInitial - Pressure_Final
+    DynamicPressure = P_current - atmos
 
-  PressureTotal = PressureInitial + DynamicPressure
+    vEXIT = math.sqrt(2.0 * DynamicPressure / 1000)
+    thrust = 1000 * Area * vEXIT ** 2
 
-  v = math.sqrt(2.0 * DynamicPressure / airdens)
+    totalmass = bottlemass + watermass
 
-  print("Velocity(in m/s): " + string(v))
-  print("DynamicPressure(in Pa): " + string(DynamicPressure))
+    netforce = thrust - (totalmass * 6)
+
+    accel = netforce / totalmass
+
+    v += accel * Time𐤃
+
+    deltaV = Area * vEXIT * Time𐤃 
+    Volumemeters = max(0, Volumemeters -deltaV)
+
+    watermass = VolumeFinal * 1000
+    
+    airvolume= VolumeFinal - Volumemeters
+
+    P_current = PressureInitial * ((VolumeFinal - VolumeInitial * litertometer)/ airvolume) * (airvolume/ VolumeFinal)
+
+
+  print("Velocity(in m/s): " + str(v))
+
+while True:
+  main()
